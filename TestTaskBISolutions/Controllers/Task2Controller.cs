43206d7db8d7b0540625532f25abaa0f0cee04b6
vova_chakalov <vova.chakalov.2003@gmail.com>
2023-08-24 00:35:42 +0300

    выполнил задание 2

--- TestTaskBISolutions/Controllers/Task2Controller.cs
+++ TestTaskBISolutions/Controllers/Task2Controller.cs
@@ -1,7 +1,32 @@
-﻿namespace TestTaskBISolutions.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TestTaskBISolutions.Models;
+namespace TestTaskBISolutions.Controllers
 {
-    public class Task2Controller
+    public class Task2Controller : Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        public IActionResult Index(string str)
+        {
+            ViewBag.Str = str;
+            return View(nameof(Index), IsPalindrome(str));
+            
+        }
+
+        public bool IsPalindrome(string str)
+        {
+            int length = str.Length;
+            for (int i = 0; i < length; i++)
+            {
+                if (str[i] != str[^(i + 1)])
+                    return false;
+            }
+            return true;
+        }
     }
 }

e49383a3d0702c9154db8737aad55865c29277b9
vova_chakalov <vova.chakalov.2003@gmail.com>
2023-08-25 00:01:47 +0300

    добавил ToDoList controller

---
+++ TestTaskBISolutions/Controllers/ToDoListController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestTaskBISolutions.Controllers
+{
+    public class ToDoListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
